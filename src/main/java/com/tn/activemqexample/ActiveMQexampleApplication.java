f11a05329851e2f437b43e6feab7f5346ebc1019
cnoriega20 <c79.noriega@gmail.com>
2021-05-23 19:35:41 -0500

    Project structure with Spring initialzr.

---
+++ src/main/java/com/tn/activemqexample/ActiveMQexampleApplication.java
@@ -0,0 +1,13 @@
+package com.tn.activemqexample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ActiveMQexampleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ActiveMQexampleApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/tn/activemqexample/ActiveMQexampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tn.activemqexample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ActiveMQexampleApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
